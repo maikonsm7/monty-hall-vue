f9f759488df6d58bc8759e32157732c9619573a3
maikonmonteiro <maikonsm43@gmail.com>
2023-07-23 18:20:01 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Problema de Monty Hall</h1>
+    <div class="inicio" v-if="!started">
+      <label for="qtdPortas">Qtd Portas</label>
+      <input type="text" name="qtdPortas" id="qtdPortas" class="texto" v-model.number="portsAmount"/>
+      <label for="portaCerta">Porta certa</label>
+      <input type="text" name="portaCerta" id="portaCerta" class="texto" v-model.number="selectedPort"/>
+      <button class="botao" @click="started = true">INICIAR</button>
+    </div>
+    <button v-if="started" @click="started = false">REINICIAR</button>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <Porta :num="i" :hasPresente="i === selectedPort"/> <!-- se for sem os dois pontos... ele interpreta uma string e não um boolean -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Porta from './components/Porta.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {Porta},
+  data: function (){
+    return {
+      started: false,
+      portsAmount: 3,
+      selectedPort: null
+    }
   }
-}
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+  font-family: Helvetica;
+}
+.inicio {
+  border: 1px solid yellow;
+  border-radius: 10px;
+  padding: 20px;
+  display: grid;
+  grid-template-rows: 40px 40px 40px;
+}
+.texto {
+  margin-left: 10px;
+  width: 5em;
+}
+.botao {
+  grid-column: span 2;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  /* justify-content: center; */
+  align-items: center;
+  color: white;
+  background-color: rgb(39, 39, 39);
+}
+.doors {
+  margin-top: 10px;
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/Presente.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="presente">
+    <div class="fita-1"></div>
+    <div class="fita-2"></div>
+    <div class="topo"></div>
+    <div class="corpo"></div>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Presente'
+}
+</script>
+
+<style>
+.presente {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    z-index: 10;
+}
+.topo {
+    width: 100px;
+    height: 25px;
+    background-color: #7aa944;
+}
+.corpo {
+    width: 90px;
+    height: 60px;
+    background-color: #5c7e32;
+}
+.fita-1 {
+    position: absolute;
+    width: 15px;
+    height: 85px;
+    background-color: red;
+}
+.fita-2 {
+    position: absolute;
+    top: 40px;
+    width: 90px;
+    height: 15px;
+    background-color: red;
+}
+</style>
